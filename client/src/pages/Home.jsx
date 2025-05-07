caa180aba25ec6e51c9a07160544abb2b7c8cfa1
Elyse Smith <isselyse@gmail.com>
2025-05-07 16:09:57 -0400

    updated home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import '../styles/global.css';
 export default function Home() {
     return(
         <><Navbar />
-        <h1>Welcome to SkillSwap!</h1>
+        <h1>Welcome to SkillSwap! Test</h1>
         <p>This is a place for fitness professionals and total newbs can come together to teach and learn anything and everything fitness related!</p>
         <p>Have you ever found yourself with a plethora of knowledge and nowhere to utilize it?</p>
         <p>Have you ever found yourself looking for information and getting it from untrustworthy sources?</p>
