0e47123ec679b42fdc89ec910a71d1a7415798ef
Elyse Smith <isselyse@gmail.com>
2025-05-07 20:57:13 -0400

    updated vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'client/dist',
-  },
   plugins: [react()],
+  build: {
+    outDir: 'dist' 
+  }
 })
